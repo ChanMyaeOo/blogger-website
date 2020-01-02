7bffb17857b075b538eefca0c0fbc4541c671554
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-01-02 21:48:39 +0630

    added blog caruosel

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
     $nav = $(".navbar");
     $toggleCollapse = $(".toogle-collapse");
 
+    //Click even for toggle menu
     $toggleCollapse.click(function(){
         $nav.toggleClass("collapse");
     })
+
+    //Owl carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        setTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$(".owl-navigation .owl-nav-prev"), $(".owl-navigation .owl-nav-next")]
+    });
 })
