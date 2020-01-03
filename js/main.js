549aa9f55b6c3f9c60f3a9a1a893ba4179346cc3
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-01-04 00:14:41 +0630

    fixed responsive screen size in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const responsive = {
-    768: {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
         items: 2
+    },
+    960: {
+        items: 3
     }
 }
 $(document).ready(function(){
